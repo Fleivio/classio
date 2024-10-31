13b348185069fb33db7efc79981f6c4571811e86
Anomalocaris <flavioborinjunior@gmail.com>
2024-10-31 13:31:15 -0300

    Login completamente implementado.

--- classio.py
+++ classio.py
@@ -1,25 +1,16 @@
 from flask import *
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def index_screen():
-    return """
-    <h1>Classio</h1>
-    <p>Bem-vindo à página inicial.</p>
-    <p>Acesse a página de <a href="/login">Login</a>.</p>
-    """
+    return render_template("index.jinja")
 
-@app.route("/login")
-def login_screen():
-    password = request.args.get('password')
-    print(password)
-    return "<h1>Login</h1>"
+@app.get("/login")
+def login_get():
+    return render_template("login.jinja")
 
-@app.route("/statistics")
-def statistics_screen():
-    return "<h1>Statistics</h1>"
-
-@app.route("/class")
-def class_screen():
-    return "<h1>Class</h1>"
+@app.post("/login")
+def login_post():
+    return "<p>You are now logged in.</p>"
