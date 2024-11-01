41516f3a6368808501408ab192521e10719908b7
Anomalocaris <flavioborinjunior@gmail.com>
2024-11-01 09:31:21 -0300

    login estático

--- classio.py
+++ classio.py
@@ -1,16 +1,28 @@
 from flask import *
-from markupsafe import escape
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route("/")
 def index_screen():
-    return render_template("index.jinja")
-
+    logged = False
+    if logged:
+        return render_template("index_logged.jinja")
+    else:
+        return render_template("index.jinja")
+    
 @app.get("/login")
 def login_get():
     return render_template("login.jinja")
 
 @app.post("/login")
 def login_post():
-    return "<p>You are now logged in.</p>"
+    username = request.form["username"]
+    password = request.form["password"]
+    if (username == "eduardo"):
+        return redirect("/")
+    else:
+        flash('Email inválido. Tente novamente.')
+        return redirect('/login')
+        
