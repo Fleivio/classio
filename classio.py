04c0583f6803e964b0b4970376497d96b1b5b6c5
Anomalocaris <flavioborinjunior@gmail.com>
2024-11-01 16:36:16 -0300

    added token cookie

--- classio.py
+++ classio.py
@@ -6,8 +6,9 @@ app.secret_key = os.urandom(24)
 
 @app.route("/")
 def index_screen():
-    logged = False
-    if logged:
+    token = request.cookies.get('auth_token')
+    if token:
+        # adicionar verificação da validade do token
         return render_template("index_logged.jinja")
     else:
         return render_template("index.jinja")
@@ -21,8 +22,17 @@ def login_post():
     username = request.form["username"]
     password = request.form["password"]
     if (username == "eduardo"):
-        return redirect("/")
+        # adicionar verificação de senha e email
+        response = make_response(redirect(url_for('index_screen')))
+        response.set_cookie('auth_token', 'default_token')
+        return response
     else:
         flash('Email inválido. Tente novamente.')
         return redirect('/login')
-        
+
+@app.route('/logout')
+def logout():
+    # Remove o cookie de autenticação
+    response = make_response(redirect(url_for('index_screen')))
+    response.set_cookie('auth_token', '', expires=0)
+    return response
